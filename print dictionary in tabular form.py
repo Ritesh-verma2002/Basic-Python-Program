89289697f5ac4fc01a9987c93053385115599784
techieritesh <riteshponty@gmail.com>
2021-05-10 23:38:59 +0530

    Tabular form of dictionary

---
+++ print dictionary in tabular form.py
@@ -0,0 +1,6 @@
+dic={'Name':'Ritesh verma','Section':'C','Age':16,"Address":'Krishna Nager'}
+for key in dic.keys():
+    print(key,end="              ")
+print()
+for value in dic.values():
+    print(value,end="              ")
